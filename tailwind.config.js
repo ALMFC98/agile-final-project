427ecb3e64730a2fcd6a6f29cc9d9a5ed4ff7290
ALMFC98 <mbarker.hfr@gmail.com>
2025-07-14 08:28:04 -0600

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
-  theme: {
-    extend: {
-      colors: {
-        navy: '#0a192f',
-        gold: '#dab756',
-        black: '#000000',
-        white: '#ffffff',
-        emerald: '#2ecc71',
-        forest: '#228B22',
-      },
-      fontFamily: {
-        sans: ['"Poppins"', 'sans-serif'],
-        display: ['"Cinzel Decorative"', 'serif'],
-      },
-    },
-  },
-  plugins: [],
+  "content": [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  "theme": {
+    "extend": {},
+    "plugins": []
+  }
 }
